ec6c6b2768fcad4bec1a166079b484af180453fe
Nishat Rahman <168478182+Nishat-Rahman-web@users.noreply.github.com>
2024-07-26 09:29:31 +0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import './App.css'
+import LoginPage from './Pages/LoginPage'
+import NotFound from './Components/Login/NotFound/NotFound'
+import { ToastContainer } from 'react-toastify'
+
+
+function App() {
+  const route = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path='/' element={<LoginPage/>}/>
+        <Route path='*' element={<NotFound/>}/>
+      </Route>
+    )
+  )
+  return (
+    <>
+      <ToastContainer />
+      <RouterProvider router={route}/>
+    </>
+  )
+}
+
+export default App
